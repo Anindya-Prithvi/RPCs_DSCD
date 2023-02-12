1389025f3d7d871b5d883cbcb650d91a52e1dc82
Andy Candy <anindya20024@iiitd.ac.in>
2023-02-12 12:40:03 +0530

    server_serving

---
+++ grpc/community_server.py
@@ -0,0 +1,43 @@
+import sys, logging
+import grpc
+from concurrent import futures
+
+import registry_server_pb2
+import registry_server_pb2_grpc
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def register_server(name, addr):
+    with grpc.insecure_channel('localhost:21337') as channel:
+        stub = registry_server_pb2_grpc.MaintainStub(channel)
+        response = stub.RegisterServer(registry_server_pb2.Server_information(name=name, addr=addr))
+        return response.value
+
+def serve(name: str, port: int):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # TODO: Add servicer for publishing and subscribing
+    # registry_server_pb2_grpc.add_MaintainServicer_to_server(Maintain(), server)
+    server.add_insecure_port('[::]:' + str(port))
+    server.start()
+    logger.info("Server started, listening on " + str(port))
+    if register_server(name, "[::]:" + str(port)):
+        logger.info("Server registered")
+    else:
+        logger.error("Server registration failed. Fatal error.")
+        exit(1)
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    #accept command line args name and port
+    logging.basicConfig()
+    if len(sys.argv) == 3:
+        name = sys.argv[1]
+        port = int(sys.argv[2])
+        logger.info("Starting server %s on port %d", name, port)
+        serve(name, port)
+    else:
+        print("Usage: python community_server.py <name> <port>")
+        sys.exit(1)
+
--- grpc/registry_server.py
+++ grpc/registry_server.py
@@ -1,18 +1,3 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""The Python implementation of the GRPC helloworld.Greeter server."""
-
 from concurrent import futures
 import logging
 
@@ -29,20 +14,13 @@ server_lim = 5
 class Maintain(registry_server_pb2_grpc.MaintainServicer):
 
     def RegisterServer(self, request, context):
-        # print(request, context.__dict__)
-        # logger.info(context.peer()); no need imo, there will be a seperate server running for server
-        # help(context)
-        
-        # logger.info(request)
-        
+        # TODO: name and addr should be unique
         new_server = registered.servers.add()
         new_server.name = "lol"
         new_server.addr = request.addr
         return registry_server_pb2.Success(value=True)
 
     def GetServerList(self, request, context):
-        # print(request, context.__dict__)
-        # logger.info(context.peer());
         book_of_5 = registry_server_pb2.Server_book()
         book_of_5.servers.extend(registered.servers[:5])
         return book_of_5
